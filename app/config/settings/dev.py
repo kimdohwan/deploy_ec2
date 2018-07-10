727eebf65de8b94b94060710b3725feb1908d5a4
dohwan <dosio0102@gmail.com>
2018-07-10 15:52:59 +0900

    default file storage set

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -16,6 +16,19 @@ WSGI_APPLICATION = 'config.wsgi.dev.application'
 DATABASES = secrets['DATABASES']
 DEBUG = True
 print(DATABASES)
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = secrets['AWS_STORAGE_BUCKET_NAME']
+AWS_DEFAULT_ACL = secrets['AWS_DEFAULT_ACL']
+AWS_S3_REGION_NAME = secrets['AWS_S3_REGION_NAME']
+AWS_S3_SIGNATURE_VERSION = secrets['AWS_S3_SIGNATURE_VERSION']
+
+INSTALLED_APPS += [
+    'storages',
+]
 # ALLOWED_HOSTS는 로컬에서는 없어도 localhost로 자동설정 됨
 # ALLOWED_HOSTS = [
 #     'localhost',
